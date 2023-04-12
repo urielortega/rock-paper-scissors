907aa41702c26610a553a1ac3eaf93e77508571d
Uriel Ortega <urielortega2509@gmail.com>
2023-04-11 18:06:17 -0600

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Uriel Ortega on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
